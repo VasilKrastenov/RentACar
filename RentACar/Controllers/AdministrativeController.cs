b6f7fc74550bfc6a3472e3e69c0842789c133109
Iva Bondzheva <ivka@dsoft-bg.com>
2023-04-22 22:51:47 +0300

    More addutions and changes to the views

---
+++ RentACar/Controllers/AdministrativeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACar.Controllers
+{
+    public class AdministrativeController : Controller
+    {
+        // GET: Administrative
+        public ActionResult AddCar()
+        {
+            return View();
+        }
+
+        public ActionResult EditCar()
+        {
+            return View();
+        }
+    }
+}
